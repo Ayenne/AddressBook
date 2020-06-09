34d6ad3c42606df477f69e5359b4b5531a04bf03
Greta Zurkowska <greta.zurkowska@gmail.com>
2020-06-09 17:58:52 +0200

    Set up ESLint.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+/**
+ * Entry point to the application
+ * @return {Element}
+*/
 function App() {
   return (
     <div className="App">
