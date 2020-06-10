33b9a5bc5cbb3858563a9e30b05ad1007887c21c
Greta Zurkowska <greta.zurkowska@gmail.com>
2020-06-10 13:28:55 +0200

    Display details of the user in the modal.

--- src/components/UserCard/index.js
+++ src/components/UserCard/index.js
@@ -1,28 +1,108 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import Popup from 'reactjs-popup';
 import './style.scss';
 
 /**
  * Renders a card with details about users.
  * @prop {Object} user
 */
-class UserCard extends Component {
+class UserCardDetails extends Component {
   render() {
     return <>
-      <div className="user-card" role='card'>
+      <div className="user-card-details" role='card'>
         <img src={this.props.user.picture.thumbnail}></img>
         <div className="details">
           <h2>{this.props.user.name.first} {this.props.user.name.last}</h2>
-          <h3>{this.props.user.login.username}</h3>
-          <h3>{this.props.user.email}</h3>
+          <p>{this.props.user.login.username}</p>
+          <p>{this.props.user.email}</p>
         </div>
       </div>
     </>;
   }
 }
 
+/**
+ * Renders content for the popup with user details.
+ * @prop {Object} user
+*/
+class UserCardPopup extends Component {
+  render() {
+    const user = this.props.user;
+    return <>
+      <div role='modal' className='card-popup'>
+        <img className="photo-popup"
+          src={user.picture.thumbnail}>
+        </img>
+        <div className="details">
+          <h2>{user.name.first} {user.name.last}</h2>
+          <p>{user.login.username}</p>
+          <p>{user.email}</p>
+          <p>
+            {user.location.street.name} {user.location.street.number}
+          </p>
+          <p>
+            {user.location.postcode} {user.location.city}
+          </p>
+          <p>{user.location.state}</p>
+          <p>phone: {user.phone} mobile: {user.cell}</p>
+        </div>
+      </div>
+    </>;
+  }
+}
+
+/**
+ * Renders a card with details about users and popup functionality.
+ * @prop {Object} user
+*/
+class UserCard extends Component {
+  render() {
+    return <>
+      <Popup trigger={
+        <div>
+          <UserCardDetails user={this.props.user} />
+        </div>
+      } modal closeOnDocumentClick
+      >
+        <UserCardPopup user={this.props.user}/>
+      </Popup>
+    </>;
+  }
+}
+
 export default UserCard;
 
+const userShape = PropTypes.shape({
+  name: PropTypes.shape({
+    first: PropTypes.string,
+    last: PropTypes.string,
+  }),
+  location: PropTypes.shape({
+    street: PropTypes.shape({
+      name: PropTypes.string,
+      number: PropTypes.number,
+    }),
+    postcode: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.number,
+    ]),
+    state: PropTypes.string,
+  }),
+  picture: PropTypes.shape({
+    thumbnail: PropTypes.string,
+  }),
+  login: PropTypes.shape({
+    username: PropTypes.string,
+  }),
+});
+
 UserCard.propTypes = {
-  user: PropTypes.object,
+  user: userShape,
+};
+UserCardPopup.propTypes = {
+  user: userShape,
+};
+UserCardDetails.propTypes = {
+  user: userShape,
 };
--- src/components/UserCard/index.test.js
+++ src/components/UserCard/index.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {render, screen} from '@testing-library/react';
+import {render, screen, fireEvent} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 import UserCard from './index';
@@ -24,4 +24,43 @@ test('Renders', () => {
   expect(screen.getByRole('card')).toHaveTextContent('Dolor');
   expect(screen.getByRole('card')).toHaveTextContent('emet@gmail.com');
   expect(screen.getByRole('img')).toHaveProperty('src', 'http://www.path.com/');
+});
+
+test('Opens Popup', () => {
+  const user = {
+    name: {
+      first: 'Lorem',
+      last: 'Ipsum',
+    },
+    login: {
+      username: 'Dolor',
+    },
+    email: 'amet@gmail.com',
+    picture: {
+      thumbnail: 'http://www.path.com/',
+    },
+    location: {
+      street: {
+        name: 'Bahnhofstrasse',
+        number: 1,
+      },
+      city: 'Boston',
+      postcode: '1234',
+      state: 'XYZ',
+    },
+    phone: '123456789',
+    cell: '987654321',
+  };
+  render(<UserCard user={user} />);
+
+  fireEvent.click(screen.getByRole('card'));
+
+  expect(screen.getByRole('modal')).toHaveTextContent('Lorem Ipsum');
+  expect(screen.getByRole('modal')).toHaveTextContent('Dolor');
+  expect(screen.getByRole('modal')).toHaveTextContent('amet@gmail.com');
+  expect(screen.getByRole('modal')).toHaveTextContent('Bahnhofstrasse 1');
+  expect(screen.getByRole('modal')).toHaveTextContent('1234 Boston');
+  expect(screen.getByRole('modal')).toHaveTextContent('XYZ');
+  expect(screen.getByRole('modal')).toHaveTextContent('phone: 123456789');
+  expect(screen.getByRole('modal')).toHaveTextContent('mobile: 987654321');
 });
