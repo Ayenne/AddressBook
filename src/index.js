ea686dc6de1e6859cb8c5e18258cd426c12df0be
Greta Zurkowska <greta.zurkowska@gmail.com>
2020-06-24 17:58:59 +0200

    Use mixins and variables to clean up styling.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {store} from './store';
 import {Provider} from 'react-redux';
-import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
